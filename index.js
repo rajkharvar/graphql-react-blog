f72068516e2b395b794f21f797e90cf36c8ea198
rajkharvar <rajkharvar1808@gmail.com>
2020-08-16 20:02:27 +0530

    🛠  minor fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers/");
 
 const server = new ApolloServer({
+  cors: false,
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
